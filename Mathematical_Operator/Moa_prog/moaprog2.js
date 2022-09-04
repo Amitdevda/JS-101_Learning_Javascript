38089eb1acf89c8cbd93835119fd1e029cf607ce
Amit Devda <akd9t7@gmail.com>
2022-09-04 16:21:15 +0530

    Mathmatical operators

---
+++ Mathematical_Operator/Moa_prog/moaprog2.js
@@ -0,0 +1,6 @@
+var one=1;
+var two=2;
+var three=3;
+var four=4;
+var five=5;
+console.log((one*2)+(two*2)+(three*2)+(four*2)+(five*2));
---
+++ Mathematical_Operator/Moa_prog/moaprog3.js
@@ -0,0 +1,7 @@
+var one=1;
+var two=2;
+var three=3;
+var four=4;
+var five=5;
+var six=6;
+console.log((one)+(two*2)+(three*3)+(four*3)+(five*2)+(six*2));
